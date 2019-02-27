43b9278d7975f908520b95931f591bf2bfab2d78
Lionel GUEZ <lguez@xebia.fr>
2019-02-28 00:37:49 +0100

    Add withdrawal feature

--- src/main/java/guez/lionel/xebia/kata/bank/domain/Account.java
+++ src/main/java/guez/lionel/xebia/kata/bank/domain/Account.java
@@ -2,14 +2,18 @@ package guez.lionel.xebia.kata.bank.domain;
 
 public class Account {
 
-    double balance;
+    private double balance;
 
-    public Account(){
+    public Account() {
         balance = 0;
     }
 
     public void deposit(double amount) {
         balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
     }
 
     public double getBalance() {
--- src/test/java/guez/lionel/xebia/kata/bank/domain/AccountTest.java
+++ src/test/java/guez/lionel/xebia/kata/bank/domain/AccountTest.java
@@ -8,10 +8,17 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class AccountTest {
 
     @Test
-    public void a_deposit_should_increase_the_balance_of_the_account(){
+    public void a_deposit_should_increase_the_balance_of_the_account() {
         Account account = new Account();
         account.deposit(30);
         assertThat(account.getBalance()).isEqualTo(30);
     }
 
+    @Test
+    public void a_withdrawal_should_decrease_the_balance_of_the_account() {
+        Account account = new Account();
+        account.deposit(30);
+        account.withdraw(10);
+        assertThat(account.getBalance()).isEqualTo(20);
+    }
 }

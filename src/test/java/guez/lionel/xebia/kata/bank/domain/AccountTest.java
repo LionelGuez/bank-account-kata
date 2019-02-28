55f5f2780ce09f805cd13bad8ba6deed12aa4d66
Lionel GUEZ <lguez@xebia.fr>
2019-02-28 01:07:28 +0100

    Very small refactoring of the tests

--- src/test/java/guez/lionel/xebia/kata/bank/domain/AccountTest.java
+++ src/test/java/guez/lionel/xebia/kata/bank/domain/AccountTest.java
@@ -1,6 +1,5 @@
 package guez.lionel.xebia.kata.bank.domain;
 
-
 import org.junit.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,16 +8,26 @@ public class AccountTest {
 
     @Test
     public void a_deposit_should_increase_the_balance_of_the_account() {
+        // GIVEN
         Account account = new Account();
+
+        // WHEN
         account.deposit(30);
+
+        // THEN
         assertThat(account.getBalance()).isEqualTo(30);
     }
 
     @Test
     public void a_withdrawal_should_decrease_the_balance_of_the_account() {
+        // GIVEN
         Account account = new Account();
         account.deposit(30);
+
+        // WHEN
         account.withdraw(10);
+
+        // THEN
         assertThat(account.getBalance()).isEqualTo(20);
     }
 }
